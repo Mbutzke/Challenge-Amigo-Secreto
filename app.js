312aa2ea3cf4c14a90c1c2b0a792513f23a4da2d
Matheus Butzke <matheusbutzke99@gmail.com>
2025-03-15 15:46:52 -0300

    Sortear um amigo na lista e adição de comentários

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaAmigos = [];
 
+// Adiciona nomes na lista
 function adicionarAmigo() {
     let input = document.getElementById("amigo");
     let nome = input.value.trim();
@@ -14,7 +15,7 @@ function adicionarAmigo() {
     input.value = ""; 
     atualizarLista();
 }
-
+// Atualiza a lista exibida na tela
 function atualizarLista() {
     let lista = document.getElementById("listaAmigos");
     lista.innerHTML = "";
@@ -23,4 +24,18 @@ function atualizarLista() {
         li.textContent = amigo;
         lista.appendChild(li);
     });
+}
+
+// Sorteia um amigo aleatório usando uma condicional para ter ao menos 1 nome para sortear
+function sortearAmigo() {
+    if (listaAmigos.length === 0) {
+        alert("A lista de amigos está vazia. Adicione pelo menos um nome antes de sortear.");
+        return;
+    }
+        
+    let indiceSorteado = Math.floor(Math.random() * listaAmigos.length);
+    let amigoSorteado = listaAmigos[indiceSorteado];
+        
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>${amigoSorteado}</li>`;
 }
